f94aa04345c00636ce0d4d58f9c59734f0d7f524
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-09-18 23:54:43 +0200

    Added ExpenseTrackerContext

---
+++ Data/ExpenseTrackerContext.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ExpenseTrackerContext : DbContext
+    {
+
+        public ExpenseTrackerContext(DbContextOptions<ExpenseTrackerContext> options) : base(options) 
+        { 
+            
+            
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Budgets> Budgets { get; set; }
+        public DbSet<Categorys> Categorys { get; set; }
+        public DbSet<Expenses> Expenses { get; set; }
+        public DbSet<PaymentMethods> PaymentMethods { get; set; }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using ExpenseTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add database
+builder.Services.AddDbContext<ExpenseTrackerContext>(options =>
+{
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ExpenseTrackerContext"));
+
+});
 
 var app = builder.Build();
 
