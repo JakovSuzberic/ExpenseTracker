32a2cb40cd84be85435148311fb5cb7cd72cdd08
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-09-19 17:57:58 +0200

    Update Models

--- Models/Categorys.cs
+++ Models/Categorys.cs
@@ -4,8 +4,8 @@ namespace ExpenseTracker.Models
 {
     public class Categorys: Users
     {
-        [Key]
-        public int? CategoryId { get; set; }
+        
+        public int? Category_Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
 
