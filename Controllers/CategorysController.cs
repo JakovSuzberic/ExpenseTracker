0d692a1f7d4d23b204868afe3828629ed6fe338d
ibrzica1 <jakov.suzberic@gmail.com>
2024-10-10 20:42:01 +0200

    Updated Controllers
    Commented every other controller thats not for mvp right now

--- Controllers/CategorysController.cs
+++ Controllers/CategorysController.cs
@@ -1,30 +1,15 @@
+
 ﻿using ExpenseTracker.Data;
 using ExpenseTracker.Models;
 using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
 
-//namespace ExpenseTracker.Controllers
-//{
-//    [ApiController]
-//    [Route("api/v1/[controller]")]
-//    public class CategorysController: ControllerBase
-//    {
 
-//        private readonly ExpenseTrackerContext _context;
-//        public CategorysController(ExpenseTrackerContext context)
-//        {
+namespace ExpenseTracker.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CategorysController: ControllerBase
 
-//            _context = context;
 
-//        }
-
-//        [HttpGet]
-//        public IActionResult Get()
-//        {
-
-//            return Ok(_context.Categorys);
-
-//        }
-
-//    }
-//}
+    }
+}
