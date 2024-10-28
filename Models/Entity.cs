d4eab3a3f60a5cb9f26030d3e53ba3ba2c808e9c
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-10-28 22:33:58 +0100

    Create Entity abstract class

---
+++ Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public abstract class Entity
+    {
+
+            [Key]
+            public int? Entity_id { get; set; }
+
+    }
+}
