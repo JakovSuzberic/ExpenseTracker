caba32792a95973176ee08b1ed9d19795d1c0504
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-10-28 22:34:06 +0100

    Created Operater

---
+++ Models/Operater.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class Operater: Entity
+    {
+
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
