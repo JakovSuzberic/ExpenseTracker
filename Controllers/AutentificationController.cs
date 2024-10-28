cc19c9e54a15503920968c216e4fc6a0fc7c681e
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-10-28 22:32:27 +0100

    Added AutentificationController and edited Program.cs
    also installed JwtBearer library

---
+++ Controllers/AutentificationController.cs
@@ -0,0 +1,66 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ExpenseTracker.Controllers
+{
+    
+
+    [ApiController]
+    [Route("api/vi/[controller]")]
+    public class AutentificationController(ExpenseTrackerContext context) : ControllerBase
+    {
+
+        private readonly ExpenseTrackerContext _context = context;
+
+        [HttpPost("token")]
+        public IActionResult GenerateToken(OperaterDTO operater)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var operBaza = _context.Operateri
+                   .Where(p => p.Email!.Equals(operater.email))
+                   .FirstOrDefault();
+
+            if (operBaza == null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, "You are not authorized, can not find operater");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(operater.password, operBaza.Password))
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, "You are not authorized, password does not match");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan i dovoljno dugačak da se može koristiti");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(8)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(jwt);
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 using ExpenseTracker.Data;
 using ExpenseTracker.Mapping;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,36 +16,97 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(sgo =>
+{ // sgo je instanca klase SwaggerGenOptions
+
+
+    // SECURITY
+
+    sgo.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Autorizacija radi tako da se prvo na ruti /api/v1/Autorizacija/token.  
+                          autorizirate i dobijete token (bez navodnika). Upišite 'Bearer' [razmak] i dobiveni token.
+                          Primjer: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2OTc3MTc2MjksImV4cCI6MTY5Nzc0NjQyOSwiaWF0IjoxNjk3NzE3NjI5fQ.PN7YPayllTrWESc6mdyp3XCQ1wp3FfDLZmka6_dAJsY'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    sgo.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+
+            },
+         new List<string>()
+        }
+    });
+
+    // END SECURITY
+
+});
+
 
 // Add database
 builder.Services.AddDbContext<ExpenseTrackerContext>(options =>
+    {
+
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ExpenseTrackerContext"));
+
+    });
+
+    builder.Services.AddCors(options =>
+    {
+
+        options.AddPolicy("CorsPolicy",
+            builder =>
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+        );
+    });
+
+    //AutoMapper
+    builder.Services.AddAutoMapper(typeof(ExpenseTrackerMappingProfile));
+
+
+//security
+// https://www.youtube.com/watch?v=mgeuh8k3I4g&ab_channel=NickChapsas
+builder.Services.AddAuthentication(x =>
 {
-
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ExpenseTrackerContext"));
-
-});
-
-builder.Services.AddCors(options =>
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
 {
-
-    options.AddPolicy("CorsPolicy",
-        builder =>
-            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-    );
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan i dovoljno duga?ak da se može koristiti")),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = false
+    };
 });
-
-//AutoMapper
-builder.Services.AddAutoMapper(typeof(ExpenseTrackerMappingProfile));
+builder.Services.AddAuthorization();
+// endsecurity
 
 var app = builder.Build();
 
 
 
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
     app.UseSwagger();
     app.UseSwaggerUI(o =>
     {
@@ -47,14 +115,19 @@ var app = builder.Build();
         o.EnableTryItOutByDefault();
 
     });
-//}
+    //}
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-app.UseAuthorization();
+    // SECURITY
+    app.UseAuthentication();
+    // ENDSECURITY
 
-app.MapControllers();
+    app.UseAuthorization();
 
-app.UseCors("CorsPolicy");
+    app.MapControllers();
 
-app.Run();
+    app.UseCors("CorsPolicy");
+
+    app.Run();
+
